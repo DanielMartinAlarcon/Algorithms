122cec6e9ec810dec3d76f7ec301fe68c900b9ec
DanielMartinAlarcon <daniel@martinalarcon.org>
2019-06-05 16:35:29 -0700

    rock_paper_scissors done

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,28 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  def rps(list1):
+    list2 = []
+    for item in list1:
+      list2.append(item + ['rock'])
+      list2.append(item + ['paper'])
+      list2.append(item + ['scissors'])
+    return list2
+  
+  starting_list = [['rock'],['paper'],['scissors']]
+  
+  if n == 0:
+    return [[]]
+  elif n == 1:
+    return starting_list
+  else:
+    list1 = starting_list
+    list2 = []
+    for i in range(1, n):
+      list2 = rps(list1)
+      list1 = list2
+
+    return list2
 
 
 if __name__ == "__main__":
