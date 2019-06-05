bad27b03017d95b92d8f26f62d3a426c0557a922
DanielMartinAlarcon <daniel@martinalarcon.org>
2019-06-05 14:03:12 -0700

    stock_prices done

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python
 
-import argparse 
+# import argparse 
 
 def find_max_profit(prices):
-  pass
-
+  current_min_price_so_far = prices[0]
+  max_profit_so_far = prices[1] - prices[0]
+  
+  for x in prices[1:]:
+    if x - current_min_price_so_far > max_profit_so_far:
+      max_profit_so_far = x - current_min_price_so_far
+    if x < current_min_price_so_far:
+      current_min_price_so_far = x
+          
+  return max_profit_so_far
+      
+  return max_profit_so_far
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
